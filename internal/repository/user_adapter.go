1bbf9a03168124411a15d88cc2035b42e2d5fd27
Duc Nguyen <minhduc140583@gmail.com>
2023-03-19 17:28:17 +0700

    Separate user_repository

--- internal/repository/user_adapter.go
+++ internal/repository/user_adapter.go
@@ -9,14 +9,6 @@ import (
 	. "go-service/internal/model"
 )
 
-type UserRepository interface {
-	Load(ctx context.Context, id string) (*User, error)
-	Create(ctx context.Context, user *User) (int64, error)
-	Update(ctx context.Context, user *User) (int64, error)
-	Patch(ctx context.Context, user map[string]interface{}) (int64, error)
-	Delete(ctx context.Context, id string) (int64, error)
-}
-
 func NewUserAdapter(db *sql.DB) *UserAdapter {
 	return &UserAdapter{DB: db}
 }
