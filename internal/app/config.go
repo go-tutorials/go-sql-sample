8fe2ea7c9c5058bf85050d47b7c780dc8d8051a6
Duc Nguyen <minhduc140583@gmail.com>
2024-06-23 23:54:25 +0700

    Remove client

--- internal/app/config.go
+++ internal/app/config.go
@@ -6,15 +6,12 @@ import (
 	mid "github.com/core-go/log/middleware"
 	"github.com/core-go/log/zap"
 	"github.com/core-go/sql"
-
-	"go-service/pkg/client"
 )
 
 type Config struct {
-	Server     core.ServerConf     `mapstructure:"server"`
-	Sql        sql.Config          `mapstructure:"sql"`
-	Client     client.ClientConfig `mapstructure:"client"`
-	Log        log.Config          `mapstructure:"log"`
-	Response   header.Config       `mapstructure:"response"`
-	MiddleWare mid.LogConfig       `mapstructure:"middleware"`
+	Server     core.ServerConf `mapstructure:"server"`
+	Sql        sql.Config      `mapstructure:"sql"`
+	Log        log.Config      `mapstructure:"log"`
+	Response   header.Config   `mapstructure:"response"`
+	MiddleWare mid.LogConfig   `mapstructure:"middleware"`
 }
